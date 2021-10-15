dcfd34be865942b3a79b54dc92991475fa2c47da
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2021-10-15 16:36:48 +0100

    feat: Complete the let and const challenge

---
+++ easy-js/let-and-const.js
@@ -0,0 +1,10 @@
+function main(r) {
+  const PI = Math.PI;
+
+  console.log((area = PI * Math.pow(r, 2)));
+  console.log((perimeter = 2 * PI * r));
+}
+
+main(2.6);
+
+// The code was manipulated a little by removing readline() since the HackerRank challenge is not letting us to call the main function in their own coding space
