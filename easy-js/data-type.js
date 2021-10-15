e5d4ca813f33281f25ab8e236e4c4d26b7e211e9
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2021-10-15 15:22:20 +0100

    feat: Complete the data type challenge on hackerrank

---
+++ easy-js/data-type.js
@@ -0,0 +1,11 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+  const firstInteger = 4;
+  const firstDecimal = 4.0;
+  const firstString = "HackerRank ";
+
+  console.log(+secondInteger + firstInteger);
+  console.log(+secondDecimal + firstDecimal);
+  console.log(firstString + secondString);
+}
+
+performOperation(12, 4.32, "is the best place to learn and practice coding!");
