7506ede5aae207cc2c1249c0cffd46ce28c6d1df
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2021-10-15 15:58:10 +0100

    feat: Complete the Arithmetic operators challenge

---
+++ easy-js/Arithmetic-operators.js
@@ -0,0 +1,14 @@
+function getArea(length, width) {
+  let area;
+  area = length * width;
+  return area;
+}
+console.log(getArea(3, 4.5));
+
+function getPerimeter(length, width) {
+  let perimeter;
+  perimeter = 2 * (length + width);
+  return perimeter;
+}
+
+console.log(getPerimeter(3, 4.5));
